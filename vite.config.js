075ca9e81ddab711240d5d2d38437a263d307351
Declan Lynch <dlynch@qtzar.com>
2025-12-17 22:24:19 -0500

    Update base path for dashboarddemo repository

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/trc/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/dashboarddemo/' : '/',
   server: {
     port: 3000,
     open: true
